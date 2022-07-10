c95bd09276e88857ad7cc53f42346593af4b3431
E <effyverse@gmail.com>
2022-07-10 04:19:42 -0400

    add to table functionality + styling

--- README.md
+++ README.md
@@ -7,20 +7,43 @@ Chrome extension Next.js template forked from [Next Chrome](https://github.com/t
 
 # Usage
 
+### Make your DB
+
+You can keep the default settings of this extension by making your DB table match the code of extension table:
+
+```
+  Title (property type: Text)  
+  URL (property type: URL)  
+  Tags (property type: Multi-select)  
+  Notes (property type: Text)
+  Added On (property type: Created time )
+```
+
+Or you can adjust the code found here `next-app/pages/index.js` to match your needs.
+
 ### Get the API credentials
 
 Sign up for a Notion API integration: https://www.notion.so/my-integrations
 
-Get your Database ID and Notion Key. 
+Get your Database ID and Notion API Key. 
 
-Add them your Notion Key to `next-app/pages/index.js`
-
+Navigate to `next-app/pages/index.js` and add your Notion Api key to:
 
 ```lang-js
   const notion = new Client({
     auth: <YOUR_NOTION_KEY/>,
     logLevel: LogLevel.DEBUG
   });
+```
+
+On the same file, add your Database ID to:
+
+```lang-js
+  try {
+    await notion.pages.create({
+      parent: {
+      database_id: <YOUR_DATABASE_ID/>,
+    },
 ```
 
 ### Run the extension
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -7,134 +7,124 @@ const IndexPage = () => {
   const [pageData, setPageData] = useState({})
   const [isSaving, setIsSaving] = useState(false)
   const [isSaved, setIsSaved] = useState(false)
- 
+
+  // establish a connection to notion
   const notion = new Client({
     auth: <YOUR_NOTION_KEY/>,
     logLevel: LogLevel.DEBUG
   });
 
+  // Save the formatted form object
+   async function saveBookmarkToNotion(bookmark) {
+     const notion = new Client({
+       auth: "secret_J4CUi8dHryTLDrrD3xzAqjiVFKrfBbnzehI8PTQjIt1",
+       logLevel: LogLevel.DEBUG
+     })
 
-(async () => {
-  console.log("retrieving DB...");
+     try {
+       await notion.pages.create({
+         parent: {
+           database_id: <YOUR_DATABASE_ID/>,
+         },
+         properties: {
+           Title: { title: [{text: { content: bookmark.title}}]},
+           URL: { url: bookmark.url },
+           Tags: { multi_select: bookmark.tags },
+           Notes: { rich_text: [{ text: {content: bookmark.notes || '-'}}]}
+         }
+       }) 
+     return true
+     } catch (error) {
+     return false
+     }
+   };
 
-  const databaseId = 'e9f5d39bf12c41559148907a5c5a5fdf'
+  // Submit handler that calls the Save function
+   async function handleSubmit(e) {
+     e.preventDefault()
+     setIsSaving(true)
 
-  const response = await notion.databases.retrieve({ database_id: databaseId });
+     const data = new FormData(e.target)
+     const bookmark = Object.fromEntries(data.entries())
+     
+     bookmark.tags = bookmark.tags
+       .split(',')
+       .filter((tag) => tag.trim().length !== 0)
+       .map((tag) => ({
+         name: tag.trim(),
+       }));
+     
+     const result = await saveBookmarkToNotion(bookmark)
 
-  console.log("db: ",response);
+     if (result) {
+       setIsSaved(true)
+     } else {
+       setIsSaving(false)
+     }
+   };
 
-})();
-
-
-
-//   async function saveBookmarkToNotion(bookmark) {
-
-//     const notion = new Client({
-//       auth: "secret_J4CUi8dHryTLDrrD3xzAqjiVFKrfBbnzehI8PTQjIt1",
-//       // auth: process.env.REACT_APP_NOTION_KEY,
-//       logLevel: LogLevel.DEBUG
-//     })
-
-//     try {
-//       await notion.pages.update({
-//         parent: {
-//           database_id: "repeated-radar-d6b.notion.site/e9f5d39bf12c41559148907a5c5a5fdf?v=13843b672de24c7783d6bc14ee18b02d"
-//           // database_id: process.env.REACT_APP_NOTION_DB,
-//         },
-//         properties: {
-//           Title: { title: [{text: { content: bookmark.title}}]},
-//           URL: { url: bookmark.url },
-//           Tags: { multi_select: bookmark.tags },
-//           Notes: { rich_text: [{ text: {content: bookmark.notes || '-'}}]}
-//         }
-//       }) 
-//     return true
-//     } catch (error) {
-//     return false
-//     }
-//   };
-
-//   async function handleSubmit(e) {
-//     e.preventDefault()
-//     setIsSaving(true)
-
-//     const data = new FormData(e.target)
-//     const bookmark = Object.fromEntries(data.entries())
-
-//     bookmark.tags = bookmark.tags
-//       .split(',')
-//       .filter((tag) => tag.trim().length !== 0)
-//       .map((tag) => ({
-//         name: tag.trim(),
-//       }))
-
-//     const result = await saveBookmarkToNotion(bookmark)
-
-//     if (result) {
-//       setIsSaved(true)
-//     } else {
-//       setIsSaving(false)
-//     }
-//   };
-
-//   useEffect(() => {
-//     chrome.tabs &&
-//       chrome.tabs.query({ 
-//         active: true, currentWindow: true }, (tabs) => {
-//           const url = tabs[0].url
-//           const title = tabs[0].title
-//           setPageData({ url, title })
-//         })
-//   }, []);
+  // get the current page URL and title
+   useEffect(() => {
+     chrome.tabs &&
+       chrome.tabs.query({ 
+         active: true, currentWindow: true }, (tabs) => {
+           const url = tabs[0].url
+           const title = tabs[0].title
+           setPageData({ url, title })
+         })
+   }, []);
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>HALLO</h1>
-      {/* <div> */}
-      {/*   {isSaved ? ( */}
-      {/*     <span>Saved</span> */}
-      {/*   ) : ( */}
-      {/*     <form onSubmit={handleSubmit}> */}
-      {/*       <div> */}
-      {/*         <label>Title</label> */}
-      {/*         <input */}
-      {/*           name="title" */} 
-      {/*           type="text" */} 
-      {/*           defaultValue={pageData.title} */} 
-      {/*           title={pageData.title} required */} 
-      {/*         /> */}
-      {/*       </div> */}
-      {/*       <div> */}
-      {/*         <label>URL</label> */}
-      {/*         <input */} 
-      {/*           name="url" */} 
-      {/*           type="url" */} 
-      {/*           defaultValue={pageData.url} */} 
-      {/*           title={pageData.url} required */} 
-      {/*         /> */}
-      {/*       </div> */}
-      {/*       <div> */}
-      {/*         <label>Tags</label> */}
-      {/*         <input name="tags" type="text" /> */}
-      {/*         <small>Separate Tags with Commas</small> */}
-      {/*       </div> */}
-      {/*       <div> */}
-      {/*         <label>Notes</label> */}
-      {/*         <input name="notes" as="textarea" rows={3} /> */}
-      {/*       </div> */}
-      {/*       <div> */}
-      {/*         <button type="submit" disabled={isSaving}> */}
-      {/*           {isSaving ? <span>Saving</span> : <span>Save</span>} */}
-      {/*         </button> */}
-      {/*       </div> */}
-      {/*     </form> */}
-      {/*   )} */}
-      {/* </div> */}
+        <h1>Add Me To DB</h1>
       </header>
+      <div>
+        {isSaved ? (
+          <span>Saved</span>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <label>Title</label>
+              <br/>
+              <input
+                name="title" 
+                type="text" 
+                defaultValue={pageData.title} 
+                title={pageData.title} required 
+              />
+            </div>
+            <div className="row">
+              <label>URL</label>
+              <br/>
+              <input 
+                name="url" 
+                type="url" 
+                defaultValue={pageData.url} 
+                title={pageData.url} required 
+              />
+            </div>
+            <div className="row">
+              <label>Tags</label>
+              <br/>
+              <input name="tags" type="text" />
+            </div>
+            <div className="row">
+              <label>Notes</label>
+              <br/>
+              <input name="notes" as="textarea" rows={3} />
+            </div>
+            <div className="row">
+              <button type="submit" disabled={isSaving}>
+                {isSaving ? <span>Saving</span> : <span>Save</span>}
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
     )
   };
 
- export default IndexPage;
+export default IndexPage;
