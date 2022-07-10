e14afa1cea4362b0ba6641e8df69b1ffa6808298
E <effyverse@gmail.com>
2022-07-10 03:24:14 -0400

    create extension that grabs db data from notion

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
-`next-chrome` is a [Next.js](https://nextjs.org/) starter project to bootstrap a new Chrome extension.
+# Save web page notes from Chrome to Notion DB
 
-[Helpful Tips for Starting a Next.js Chrome Extension | CSS-Tricks](https://css-tricks.com/nextjs-chrome-extension-starter/)
+Made for fanboys of Raymond Hettinger.
 
-```sh
+Chrome extension Next.js template forked from [Next Chrome](https://github.com/thomaswang/next-chrome)
+
+
+# Usage
+
+### Get the API credentials
+
+Sign up for a Notion API integration: https://www.notion.so/my-integrations
+
+Get your Database ID and Notion Key. 
+
+Add them your Notion Key to `next-app/pages/index.js`
+
+
+```lang-js
+  const notion = new Client({
+    auth: <YOUR_NOTION_KEY/>,
+    logLevel: LogLevel.DEBUG
+  });
+```
+
+### Run the extension
+
+```lang-js
 cd next-app
 
-yarn # run once
+yarn 
 
-yarn build # on macOS
+yarn build
 yarn build:linux # on Linux
 ```
 
-![Screenshot](./screenshot.png)
+Go to `chrome://extensions/` and select **Load unpacked**
+
+Choose the `extension` folder.
+
+Your extension should appear!
+
+
+# Debugging
+
+Right click on the extension icon and select `inspect popup` for the console
+
+
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,68 +1,140 @@
 import styles from "../styles/Home.module.css";
+import { Client, LogLevel } from '@notionhq/client'
+import { useEffect, useState } from 'react'
 
 const IndexPage = () => {
+
+  const [pageData, setPageData] = useState({})
+  const [isSaving, setIsSaving] = useState(false)
+  const [isSaved, setIsSaved] = useState(false)
+ 
+  const notion = new Client({
+    auth: <YOUR_NOTION_KEY/>,
+    logLevel: LogLevel.DEBUG
+  });
+
+
+(async () => {
+  console.log("retrieving DB...");
+
+  const databaseId = 'e9f5d39bf12c41559148907a5c5a5fdf'
+
+  const response = await notion.databases.retrieve({ database_id: databaseId });
+
+  console.log("db: ",response);
+
+})();
+
+
+
+//   async function saveBookmarkToNotion(bookmark) {
+
+//     const notion = new Client({
+//       auth: "secret_J4CUi8dHryTLDrrD3xzAqjiVFKrfBbnzehI8PTQjIt1",
+//       // auth: process.env.REACT_APP_NOTION_KEY,
+//       logLevel: LogLevel.DEBUG
+//     })
+
+//     try {
+//       await notion.pages.update({
+//         parent: {
+//           database_id: "repeated-radar-d6b.notion.site/e9f5d39bf12c41559148907a5c5a5fdf?v=13843b672de24c7783d6bc14ee18b02d"
+//           // database_id: process.env.REACT_APP_NOTION_DB,
+//         },
+//         properties: {
+//           Title: { title: [{text: { content: bookmark.title}}]},
+//           URL: { url: bookmark.url },
+//           Tags: { multi_select: bookmark.tags },
+//           Notes: { rich_text: [{ text: {content: bookmark.notes || '-'}}]}
+//         }
+//       }) 
+//     return true
+//     } catch (error) {
+//     return false
+//     }
+//   };
+
+//   async function handleSubmit(e) {
+//     e.preventDefault()
+//     setIsSaving(true)
+
+//     const data = new FormData(e.target)
+//     const bookmark = Object.fromEntries(data.entries())
+
+//     bookmark.tags = bookmark.tags
+//       .split(',')
+//       .filter((tag) => tag.trim().length !== 0)
+//       .map((tag) => ({
+//         name: tag.trim(),
+//       }))
+
+//     const result = await saveBookmarkToNotion(bookmark)
+
+//     if (result) {
+//       setIsSaved(true)
+//     } else {
+//       setIsSaving(false)
+//     }
+//   };
+
+//   useEffect(() => {
+//     chrome.tabs &&
+//       chrome.tabs.query({ 
+//         active: true, currentWindow: true }, (tabs) => {
+//           const url = tabs[0].url
+//           const title = tabs[0].title
+//           setPageData({ url, title })
+//         })
+//   }, []);
+
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <img
-              src="assets/vercel.svg"
-              alt="Vercel Logo"
-              width={72}
-              height={16}
-            />
-          </span>
-        </a>
-      </footer>
+    <div className="App">
+      <header className="App-header">
+        <h1>HALLO</h1>
+      {/* <div> */}
+      {/*   {isSaved ? ( */}
+      {/*     <span>Saved</span> */}
+      {/*   ) : ( */}
+      {/*     <form onSubmit={handleSubmit}> */}
+      {/*       <div> */}
+      {/*         <label>Title</label> */}
+      {/*         <input */}
+      {/*           name="title" */} 
+      {/*           type="text" */} 
+      {/*           defaultValue={pageData.title} */} 
+      {/*           title={pageData.title} required */} 
+      {/*         /> */}
+      {/*       </div> */}
+      {/*       <div> */}
+      {/*         <label>URL</label> */}
+      {/*         <input */} 
+      {/*           name="url" */} 
+      {/*           type="url" */} 
+      {/*           defaultValue={pageData.url} */} 
+      {/*           title={pageData.url} required */} 
+      {/*         /> */}
+      {/*       </div> */}
+      {/*       <div> */}
+      {/*         <label>Tags</label> */}
+      {/*         <input name="tags" type="text" /> */}
+      {/*         <small>Separate Tags with Commas</small> */}
+      {/*       </div> */}
+      {/*       <div> */}
+      {/*         <label>Notes</label> */}
+      {/*         <input name="notes" as="textarea" rows={3} /> */}
+      {/*       </div> */}
+      {/*       <div> */}
+      {/*         <button type="submit" disabled={isSaving}> */}
+      {/*           {isSaving ? <span>Saving</span> : <span>Save</span>} */}
+      {/*         </button> */}
+      {/*       </div> */}
+      {/*     </form> */}
+      {/*   )} */}
+      {/* </div> */}
+      </header>
     </div>
-  );
-};
+    )
+  };
 
-export default IndexPage;
+ export default IndexPage;
